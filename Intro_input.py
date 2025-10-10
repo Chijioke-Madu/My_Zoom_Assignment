505cce0a33a69b41c4cfe06c54f7226b3c9974ab
Chijioke Madu <147303494+SaltCJ@users.noreply.github.com>
2025-10-10 04:01:54 +0100

    Delete Intro_input.py

--- Intro_input.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your Name: ")
-age = input("Enter your Age:")
-gender = input("Enter your Gender: ")
-
-print("Hello there, {}!".format(name.title()))
-print("you are, {}".format(age.title()))
-print("and you happen to be a strong {}.".format(gender.title()))
